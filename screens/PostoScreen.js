6466ffd6f81f2f5a301f0ab657a83651c1b7f599
Leticia35728 <leticiade12souzaa@outlook.com>
2024-04-29 21:41:17 -0300

    adicionando função gasolina.

--- screens/PostoScreen.js
+++ screens/PostoScreen.js
@@ -1,3 +1,24 @@
 import React from 'react';
 import { View, Text, Button, TextInput, SafeAreaView, Image, StyleSheet } from 'react-native';
 
+
+const precoGasolina =parseFloat(document.getElementById("gasolina").value);
+const precoAlcool = parseFloat(document.getElementById("alcool").value);
+
+if (isNaN(precoGasolina)) {
+    msgResultado.innerHTML = "Preço da Gasolina inválido";
+    return;
+}
+
+if (isNaN(precoAlcool)) {
+    msgResultado.innerHTML = "Preço do Álcool inválido";
+    return;
+}
+
+console.log(precoAlcool / precoGasolina);
+
+if ((precoAlcool / precoGasolina) >= 0.7)
+    msgResultado.innerHTML = "Abasteca com Gasolina";
+else
+    msgResultado.innerHTML = "Abasteca com Álcool";
+
